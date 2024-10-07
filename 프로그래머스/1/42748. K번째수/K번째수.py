2d8814b9b33c711d33022d5e3ca121b5b22498cc
jun_code <101074298+junhyeok0703@users.noreply.github.com>
2024-10-07 23:24:45 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,10 +1,10 @@
 def solution(array, commands):
-    answer = []
-    for command in commands:
-        start = command[0]-1
-        end = command[1]
-        choice = command[2]
-        sclice_arr = array[start:end]
-        sorted_arr = sorted(sclice_arr)
-        answer.append(sorted_arr[choice-1])
+    answer=[]
+    for i in commands:
+        start = i[0]-1
+        end = i[1]
+        a= array[start:end]
+        a.sort()
+        idx = i[2]
+        answer.append(a[idx-1])
     return answer
