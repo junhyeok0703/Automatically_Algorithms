daa50e244c55cbc9bbb9a4ddf45b996fa8e36d9e
jun_code <101074298+junhyeok0703@users.noreply.github.com>
2024-10-24 17:01:07 +0900

    [level 1] Title: 둘만의 암호, Time: 0.45 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -2,12 +2,13 @@ def solution(s, skip, index):
     answer=''
     for i in s:
         cnt = index
-        while cnt>0:
-            if i=='z':
+        while cnt >0:
+            if i == 'z':
                 i='a'
             else:
                 i = chr(ord(i)+1)
             if i not in skip:
-                cnt-=1
-        answer+=i
+                    cnt-=1
+                    
+        answer+=i        
     return answer
