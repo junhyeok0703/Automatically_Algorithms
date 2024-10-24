79d4bf642245a1763c3c4254dc55d1196b15921a
jun_code <101074298+junhyeok0703@users.noreply.github.com>
2024-10-24 15:37:47 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,12 +1,8 @@
 import math
 def solution(numer1, denom1, numer2, denom2):
-    answer = []
-    d = denom1 * denom2
-    n1 = numer1*denom2
-    n2 = numer2*denom1
-    n3 = n1+n2
-    gcd = math.gcd(n3,d)
-    if gcd>0:
-        a = n3//gcd
-        b = d//gcd
+    b = denom1*denom2
+    a = numer1*denom2 + numer2*denom1
+    c = math.gcd(a,b)
+    a//=c
+    b//=c
     return [a,b]
