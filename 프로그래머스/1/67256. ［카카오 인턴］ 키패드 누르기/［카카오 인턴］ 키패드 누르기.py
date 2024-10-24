643f0d35fa45c593bca4e7909ae8fb92569b2da6
jun_code <101074298+junhyeok0703@users.noreply.github.com>
2024-10-24 15:29:33 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.63 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,35 +1,37 @@
 def solution(numbers, hand):
     answer = ''
-    keypad = {
-        1: (0, 0), 2: (0, 1), 3: (0, 2),
-        4: (1, 0), 5: (1, 1), 6: (1, 2),
-        7: (2, 0), 8: (2, 1), 9: (2, 2),
-        '*': (3, 0), 0: (3, 1), '#': (3, 2)
-    }
-    lq = keypad['*']
-    rq = keypad['#']
-    for num in numbers:
-        if num in [1,4,7]:
+    phonenum = {1:(0,0),2:(0,1),3:(0,2),
+               4:(1,0),5:(1,1),6:(1,2),
+               7:(2,0),8:(2,1),9:(2,2),
+               '*':(3,0),0:(3,1),'#':(3,2)}
+    
+    lq = phonenum['*']
+    rq = phonenum['#']
+    
+    for i in numbers:
+        if i in [1,4,7]:
             answer+='L'
-            lq=keypad[num]
-        elif num in [3,6,9]:
+            lq = phonenum[i]
+        elif i in [3,6,9]:
             answer+='R'
-            rq=keypad[num]
+            rq = phonenum[i]
         else:
-            targetnum = keypad[num]
-            left_dist = abs(lq[0] - targetnum[0]) + abs(lq[1] - targetnum[1])
-            right_dist = abs(rq[0] - targetnum[0]) + abs(rq[1] - targetnum[1])
-            if left_dist<right_dist:
-                answer+='L'
-                lq=targetnum
-            elif left_dist>right_dist:
+            # 지금 i와 lq와 rq의 상대적 거리 구하기
+            ll = abs(lq[0]-phonenum[i][0]) + abs(lq[1]-phonenum[i][1])
+            print(ll)
+            rr = abs(rq[0]-phonenum[i][0]) + abs(rq[1]-phonenum[i][1])
+            print(rr)
+            if ll>rr:
                 answer+='R'
-                rq=targetnum
-            elif left_dist==right_dist:
+                rq=phonenum[i]
+            elif ll<rr:
+                answer+='L'
+                lq=phonenum[i]
+            elif ll==rr:
                 if hand=='right':
                     answer+='R'
-                    rq=targetnum
+                    rq=phonenum[i]
                 else:
                     answer+='L'
-                    lq=targetnum
+                    lq=phonenum[i]
     return answer
