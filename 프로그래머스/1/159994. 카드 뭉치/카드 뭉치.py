405f4b158816ffcec87791f292c01f39482a0c16
jun_code <101074298+junhyeok0703@users.noreply.github.com>
2025-08-23 17:17:15 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.06 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,11 @@
+def solution(cards1, cards2, goal):
+    for g in goal:
+        if cards1 and cards1[0] == g: # 원소가 있고 cards1이 g랑 같을경우 cards
+            cards1.pop(0)
+        elif cards2 and cards2[0] == g: 
+            cards2.pop(0)
+        else:
+            return "No"
+    return "Yes"
+    
+    
